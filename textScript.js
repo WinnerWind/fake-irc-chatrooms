221a3969dbdd9579f3a41b5d08a183a2cf1b5c40
WinnerWind <71366717+WinnerWind@users.noreply.github.com>
2025-06-18 08:47:01 +0530

    Send messages from individual "messageGroups".
    This allows us to script individual conversastions.

--- textScript.js
+++ textScript.js
@@ -1,4 +1,4 @@
-const ircMessages = ["Message1","Message2","Message3"]
+const ircMessages = [["Message1","Message2","Message3"],["a","b","c"],["1","2","3"]]
 
 var ircMessageDisplay = document.getElementById("mainChat")
 
@@ -6,19 +6,24 @@ let lastIndex = -1; // Prevents immediate repeats.
 
 startMessages()
 
-function sendNewMessage(){
+async function sendNewMessage(){
     let randomIndex;
     do {
         randomIndex = Math.floor(Math.random() * ircMessages.length);
     } while (randomIndex === lastIndex && ircMessages.length > 1); // Ensure messages array has more than 1 item to avoid infinite loop
 
-    const message = ircMessages[randomIndex];
-    ircMessageDisplay.textContent += message+"\n"; // Line break required for proper rendering.
+    const messageGroup = ircMessages[randomIndex];
+
+    for (let messageIndex in messageGroup) {
+        ircMessageDisplay.textContent += messageGroup[messageIndex]+"\n"; // Line break required for proper rendering.
+        await delay(1000) //TODO Randomize this
+    }
+
     lastIndex = randomIndex; // Update the last displayed index
 }
 
-function startMessages() {
-  sendNewMessage() //We want to send a new message
+async function startMessages() {
+  await sendNewMessage() //We want to send a new message
 
   console.log("Function executed at:", new Date().toLocaleTimeString());
 
@@ -26,10 +31,18 @@ function startMessages() {
   const minDelaySeconds = 0.1; // Minimum delay in seconds
   const maxDelaySeconds = 1; // Maximum delay in seconds
 
-  const randomDelayMilliseconds = Math.floor(Math.random() * (maxDelaySeconds - minDelaySeconds + 1) + minDelaySeconds) * 1000;
+  const randomDelayMilliseconds = getRandomIndex(minDelaySeconds,maxDelaySeconds) * 1000;
 
   console.log("Next execution in:", randomDelayMilliseconds / 1000, "seconds");
 
   // Schedule the next execution
-  setTimeout(runRandomly, randomDelayMilliseconds);
+  setTimeout(startMessages, randomDelayMilliseconds);
+}
+
+function getRandomIndex(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
